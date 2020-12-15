29ab83a8c8b7fcc8a0712ffaa587e2b50c93f478
domanoz <mikolaj-domanski@wp.pl>
2020-12-15 18:52:25 +0100

    Initial AmbonaApp commit

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
-
+import {UserController} from '../app/controllers/userController'
 const router = Router();
 
+router.get('/', UserController.listAllUsers);
+router.post('/', UserController.addUser);
 
 export default router;
